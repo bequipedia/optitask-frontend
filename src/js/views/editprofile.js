abd535b77206cac2cebffc0815310e329f1da069
thomgasa <thomasjgamboas@gmail.com>
2021-02-23 02:44:22 -0400

    listo cambio de estructura del Grid de las vistas income y expenses

--- src/js/views/editprofile.js
+++ src/js/views/editprofile.js
@@ -7,8 +7,8 @@ export const EditProfile = props => {
 	return (
 		<div className="container-fluid">
 			<div className="row d-flex flex-row">
-				<div className="col-2">Hola soy columna izq</div>
-				<div className="col-8 bg-dark text-white">
+				<div className="col-2">Hola soy columna izquierda</div>
+				<div className="col-8 bg-light text-black">
 					<div className="row row-cols-2">
 						<div className="col">CELDA 1</div>
 						<div className="col">CELDA 2</div>
@@ -16,7 +16,7 @@ export const EditProfile = props => {
 						<div className="col">CELDA 4</div>
 					</div>
 				</div>
-				<div className="col-2">Hola soy columna izq</div>
+				<div className="col-2">Hola soy columna derecha</div>
 			</div>
 		</div>
 	);
